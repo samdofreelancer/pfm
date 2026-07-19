d8dc71681d47de7bb782019a2c72027594b4a3e8
ginseng1000years <69388936+ginseng1000years@users.noreply.github.com>
2026-07-19 13:05:11 +0700

    Update UpdateProfileRequest.java

--- backend/pfm-api/src/main/java/com/pfm/api/dto/request/UpdateProfileRequest.java
+++ backend/pfm-api/src/main/java/com/pfm/api/dto/request/UpdateProfileRequest.java
@@ -1,10 +1,10 @@
 package com.pfm.api.dto.request;
 
 import jakarta.validation.constraints.Size;
-import lombok.Value;
+import lombok.Data;
 
-@Value
+@Data
 public class UpdateProfileRequest {
     @Size(min = 2, max = 100, message = "Full name must be between 2 and 100 characters")
-    String fullName;
+    private String fullName;
 }
